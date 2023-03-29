22a75f2d0ee7faa7ccd71a73417ee4ae6ecdaf94
NhuongDT <inftech.ts91@gmail.com>
2023-03-29 21:20:05 +0700

    add Repository (HangHoa)

---
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/HangHoa/HangHoa/Repository/IHangHoaRepository.cs
@@ -0,0 +1,18 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.Domain.Repositories;
+using BanHangBeautify.Data.Entities;
+using BanHangBeautify.HangHoa.HangHoa.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanHangBeautify.HangHoa.HangHoa.Repository
+{
+    public interface IHangHoaRepository : IRepository<DM_HangHoa, Guid>
+    {
+        Task<PagedResultDto<HangHoaDto>> GetDMHangHoa(HangHoaPagedResultRequestDto input, int? tenantId);
+        Task<string> GetProductCode(int loaiHangHoa, int? tenantId);
+    }
+}
