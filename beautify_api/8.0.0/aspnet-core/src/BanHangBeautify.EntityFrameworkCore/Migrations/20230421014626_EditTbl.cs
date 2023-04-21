6ecd9aed441f7e94df946d0e54ffa95d46f0bbbc
NhuongDT <inftech.ts91@gmail.com>
2023-04-21 08:47:32 +0700

    add migration

---
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.EntityFrameworkCore/Migrations/20230421014626_EditTbl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BanHangBeautify.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditTbl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ID_Parent",
+                table: "DM_NhomHangHoa",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ID_Parent",
+                table: "DM_NhomHangHoa");
+        }
+    }
+}
