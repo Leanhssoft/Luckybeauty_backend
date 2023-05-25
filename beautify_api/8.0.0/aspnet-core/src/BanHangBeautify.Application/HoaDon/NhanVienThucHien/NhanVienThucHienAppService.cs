8dc1b0b2bc0f1f8bde7908cf1268d6bd6f0bff1c
NhuongDT <inftech.ts91@gmail.com>
2023-05-25 14:50:35 +0700

    - nvThucHien dichvu (ver1)

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/HoaDon/NhanVienThucHien/NhanVienThucHienAppService.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/HoaDon/NhanVienThucHien/NhanVienThucHienAppService.cs
@@ -1,11 +1,14 @@
 ﻿using Abp.Application.Services.Dto;
 using Abp.Authorization;
 using Abp.Domain.Repositories;
+using Abp.EntityFrameworkCore.Repositories;
 using BanHangBeautify.Authorization;
 using BanHangBeautify.Entities;
+using BanHangBeautify.HoaDon.HoaDonChiTiet.Dto;
 using BanHangBeautify.HoaDon.NhanVienThucHien.Dto;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,74 +18,114 @@ using System.Threading.Tasks;
 namespace BanHangBeautify.HoaDon.NhanVienThucHien
 {
     [AbpAuthorize(PermissionNames.Pages_NhanVienThucHien)]
-    public class NhanVienThucHienAppService:SPAAppServiceBase
+    public class NhanVienThucHienAppService : SPAAppServiceBase
     {
-        private readonly IRepository<BH_NhanVienThucHien, Guid> _repository;
+        private readonly IRepository<BH_NhanVienThucHien, Guid> _nvThucHien;
         public NhanVienThucHienAppService(IRepository<BH_NhanVienThucHien, Guid> repository)
         {
-            _repository = repository;
+            _nvThucHien = repository;
         }
-        public async Task<NhanVienThucHienDto> CreateOrEdit(CreateOrEditNhanVienThucHienDto input)
+        public async Task<bool> InsertListNVThucHien_DichVu(List<HoaDonChiTietDto> lstCTHD)
         {
-            var checkExist = await _repository.FirstOrDefaultAsync(x => x.Id == input.Id);
-            if (checkExist != null)
+            try
             {
-                return await Create(input);
+                List<BH_NhanVienThucHien> lstNVTH = new List<BH_NhanVienThucHien>();
+                foreach (var cthd in lstCTHD)
+                {
+                    foreach (var nvth in cthd.BH_NhanVienThucHien)
+                    {
+                        BH_NhanVienThucHien nvNew = ObjectMapper.Map<BH_NhanVienThucHien>(nvth);
+                        nvNew.Id = Guid.NewGuid();
+                        nvNew.TenantId = AbpSession.TenantId ?? 1;
+                        nvNew.CreatorUserId = AbpSession.UserId;
+                        nvNew.CreationTime = DateTime.Now;
+                        lstNVTH.Add(nvNew);
+                    }
+                }
+                await _nvThucHien.InsertRangeAsync(lstNVTH);
+                return true;
             }
-            return await Update(input, checkExist);
+            catch (Exception)
+            {
+                return false;
+            }
         }
-        [NonAction]
-        public async Task<NhanVienThucHienDto> Create(CreateOrEditNhanVienThucHienDto input)
+
+        public async Task<bool> UpdateNVThucHienDichVu([FromBody] JObject data, Guid idHoaDonChiTiet)
         {
-            NhanVienThucHienDto result = new NhanVienThucHienDto();
-            BH_NhanVienThucHien data = new BH_NhanVienThucHien();
-            data = ObjectMapper.Map<BH_NhanVienThucHien>(input);
-            data.Id = Guid.NewGuid();
-            data.CreationTime = DateTime.Now;
-            data.IdHoaDonChiTiet = input.IdChiTietHoaDon;
-            data.CreatorUserId = AbpSession.UserId;
-            data.TenantId = AbpSession.TenantId ?? 1;
-            data.IsDeleted = false;
-            await _repository.InsertAsync(data);
-            result = ObjectMapper.Map<NhanVienThucHienDto>(input);
-            return result;
-        }
-        [NonAction]
-        public async Task<NhanVienThucHienDto> Update(CreateOrEditNhanVienThucHienDto input, BH_NhanVienThucHien oldData)
-        {
-            NhanVienThucHienDto result = new NhanVienThucHienDto();
-            oldData.IdNhanVien = input.IdNhanVien;
-            oldData.IdHoaDon = input.IdHoaDon;
-            oldData.IdQuyHoaDon = input.IdQuyHoaDon;
-            oldData.IdHoaDonChiTiet = input.IdChiTietHoaDon;
-            oldData.PTChietKhau = input.PTChietKhau;
-            oldData.TienChietKhau = input.TienChietKhau;
-            oldData.HeSo = input.HeSo;
-            oldData.TinhHoaHongTruocCK = input.TinhHoaHongTruocCK;
-            oldData.LoaiChietKhau = input.LoaiChietKhau;
-            oldData.LastModificationTime = DateTime.Now;
-            oldData.LastModifierUserId = AbpSession.UserId;
-            await _repository.UpdateAsync(oldData);
-            result = ObjectMapper.Map<NhanVienThucHienDto>(oldData);
-            return result;
+            try
+            {
+                List<NhanVienThucHienDto> lstNV = data["NhanViens"].ToObject<List<NhanVienThucHienDto>>();
+
+                // upadate status & add again
+                _nvThucHien.GetAllIncluding(x => x.IdHoaDonChiTiet == idHoaDonChiTiet).ToList()
+                    .ForEach(x => { x.IsDeleted = true; x.DeletionTime = DateTime.Now; x.DeleterUserId = AbpSession.UserId; });
+
+                List<BH_NhanVienThucHien> lstNVAdd = new();
+                foreach (var item in lstNV)
+                {
+                    var objNew = ObjectMapper.Map<BH_NhanVienThucHien>(item);
+                    objNew.Id = Guid.NewGuid();
+                    objNew.CreationTime = DateTime.Now;
+                    objNew.IdHoaDonChiTiet = idHoaDonChiTiet;
+                    objNew.CreatorUserId = AbpSession.UserId;
+                    objNew.TenantId = AbpSession.TenantId ?? 1;
+                    objNew.IsDeleted = false;
+                    lstNVAdd.Add(objNew);
+                }
+                await _nvThucHien.InsertRangeAsync(lstNVAdd);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
         [HttpPost]
         public async Task<NhanVienThucHienDto> Delete(Guid id)
         {
-            var data = await _repository.FirstOrDefaultAsync(x => x.Id == id);
+            var data = await _nvThucHien.FirstOrDefaultAsync(x => x.Id == id);
             if (data != null)
             {
                 data.IsDeleted = true;
                 data.DeletionTime = DateTime.Now;
                 data.DeleterUserId = AbpSession.UserId;
-                await _repository.UpdateAsync(data);
+                await _nvThucHien.UpdateAsync(data);
                 return ObjectMapper.Map<NhanVienThucHienDto>(data);
             }
             return new NhanVienThucHienDto();
         }
+
+        public bool DeleteNVThucHienDichVu(Guid idHoaDonChiTiet)
+        {
+            try
+            {
+                _nvThucHien.GetAllIncluding(x => x.IdHoaDonChiTiet == idHoaDonChiTiet).ToList()
+                  .ForEach(x => { x.IsDeleted = true; x.DeletionTime = DateTime.Now; x.DeleterUserId = AbpSession.UserId; });
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        } 
+        public bool DeleteNVThucHienHoaDon(Guid idHoaDon)
+        {
+            try
+            {
+                _nvThucHien.GetAllIncluding(x => x.IdHoaDon == idHoaDon).ToList()
+                  .ForEach(x => { x.IsDeleted = true; x.DeletionTime = DateTime.Now; x.DeleterUserId = AbpSession.UserId; });
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
         public async Task<CreateOrEditNhanVienThucHienDto> GetForEdit(Guid id)
         {
-            var data = await _repository.FirstOrDefaultAsync(x => x.Id == id);
+            var data = await _nvThucHien.FirstOrDefaultAsync(x => x.Id == id);
             if (data != null)
             {
                 return ObjectMapper.Map<CreateOrEditNhanVienThucHienDto>(data);
@@ -94,7 +137,7 @@ namespace BanHangBeautify.HoaDon.NhanVienThucHien
             input.SkipCount = input.SkipCount > 0 ? input.SkipCount * input.MaxResultCount : 0;
             input.Keyword = string.IsNullOrEmpty(input.Keyword) ? "" : input.Keyword;
             PagedResultDto<NhanVienThucHienDto> result = new PagedResultDto<NhanVienThucHienDto>();
-            var lstData = await _repository.GetAll().Where(x => x.IsDeleted == false && x.TenantId == (AbpSession.TenantId ?? 1)).OrderByDescending(x => x.CreationTime).ToListAsync();
+            var lstData = await _nvThucHien.GetAll().Where(x => x.IsDeleted == false && x.TenantId == (AbpSession.TenantId ?? 1)).OrderByDescending(x => x.CreationTime).ToListAsync();
             result.TotalCount = lstData.Count;
             lstData = lstData.Skip(input.SkipCount).Take(input.MaxResultCount).ToList();
             result.Items = ObjectMapper.Map<List<NhanVienThucHienDto>>(lstData);
