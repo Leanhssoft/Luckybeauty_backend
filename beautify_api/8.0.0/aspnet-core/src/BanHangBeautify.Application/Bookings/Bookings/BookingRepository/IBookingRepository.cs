c9abd2e80f12b58ea319de2da3408efd7e8a1a8e
NhuongDT <inftech.ts91@gmail.com>
2023-07-19 14:43:39 +0700

    add func GetKhachHang_Booking

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/Bookings/Bookings/BookingRepository/IBookingRepository.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/Bookings/Bookings/BookingRepository/IBookingRepository.cs
@@ -10,5 +10,6 @@ namespace BanHangBeautify.Bookings.Bookings.BookingRepository
     public interface IBookingRepository
     {
         Task<List<BookingGetAllItemDto>> GetAllBooking(PagedBookingResultRequestDto input,int tenantId,DateTime timeFrom,DateTime timeTo);
+        Task<List<BookingDetailDto>> GetKhachHang_Booking(BookingRequestDto input);
     }
 }
