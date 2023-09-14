629d7921c4b299fa92555b7f37ee0e0f18b26669
NhuongDT <inftech.ts91@gmail.com>
2023-09-14 17:04:01 +0700

    - add migration: tblUser(add IdChiNhanhMacDinh)

---
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.EntityFrameworkCore/SP_Migrations/20230914085141_AddColumn_IdChiNhanhtoTblUser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BanHangBeautify.SPMigrations
+{
+    /// <inheritdoc />
+    public partial class AddColumnIdChiNhanhtoTblUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "IdChiNhanhMacDinh",
+                table: "AbpUsers",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+              name: "IX_AbpUsers_IdChiNhanhMacDinh",
+              table: "AbpUsers",
+              column: "IdChiNhanhMacDinh");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AbpUsers_DM_ChiNhanh_IdChiNhanhMacDinh",
+                table: "AbpUsers",
+                column: "IdChiNhanhMacDinh",
+                principalTable: "DM_ChiNhanh",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+               name: "FK_AbpUsers_DM_ChiNhanh_IdChiNhanhMacDinh",
+               table: "AbpUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AbpUsers_IdChiNhanhMacDinh",
+                table: "AbpUsers");
+
+            migrationBuilder.DropColumn(
+              name: "IdChiNhanhMacDinh",
+              table: "AbpUsers");
+        }
+    }
+}
