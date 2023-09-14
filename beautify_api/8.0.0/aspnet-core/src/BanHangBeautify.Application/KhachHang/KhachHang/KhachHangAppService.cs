a894162c0c97f3e13f7ed77fb9dbdec51279f9ae
NhuongDT <inftech.ts91@gmail.com>
2023-09-14 15:15:25 +0700

    - add function: DeleteMultipleCustomer, ChuyenNhomKhachHang

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/KhachHang/KhachHang/KhachHangAppService.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/KhachHang/KhachHang/KhachHangAppService.cs
@@ -3,6 +3,7 @@ using Abp.Authorization;
 using Abp.Domain.Repositories;
 using Abp.Domain.Uow;
 using BanHangBeautify.Authorization;
+using BanHangBeautify.Data.Entities;
 using BanHangBeautify.Entities;
 using BanHangBeautify.KhachHang.KhachHang.Dto;
 using BanHangBeautify.KhachHang.KhachHang.Exporting;
@@ -131,6 +132,28 @@ namespace BanHangBeautify.KhachHang.KhachHang
                 result = ObjectMapper.Map<KhachHangDto>(delete);
             }
             return result;
+        }
+        [HttpPost]
+        public async Task DeleteMultipleCustomer(List<Guid> lstId)
+        {
+            _repository.GetAllList(x => lstId.Contains(x.Id)).ForEach(x =>
+            {
+                x.TrangThai = 0;
+                x.IsDeleted = true;
+                x.DeleterUserId = AbpSession.UserId;
+                x.DeletionTime = DateTime.Now;
+            });
+            // todo remove image in google api
+        }
+        [HttpPost]
+        public async Task ChuyenNhomKhachHang(List<Guid> lstIdKhachHang, Guid idNhomKhach)
+        {
+            _repository.GetAllList(x => lstIdKhachHang.Contains(x.Id)).ForEach(x =>
+            {
+                x.IdNhomKhach = idNhomKhach;
+                x.LastModifierUserId = AbpSession.UserId;
+                x.LastModificationTime = DateTime.Now;
+            });
         }
         public async Task<KhachHangDetailDto> GetKhachHangDetail(Guid id)
         {
