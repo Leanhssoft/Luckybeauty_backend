3dce8f1ed33a13c3bc558b4ce9ac35f969580efc
NhuongDT <inftech.ts91@gmail.com>
2023-09-15 17:09:46 +0700

    - add UserRoleAppService

---
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/Roles/Dto/UserRoleDto.cs
@@ -0,0 +1,20 @@
+﻿using Abp.Application.Editions;
+using Abp.Authorization.Users;
+using Abp.AutoMapper;
+using BanHangBeautify.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanHangBeautify.Roles.Dto
+{
+    public class CreateOrUpdateUserRoleDto: UserRole
+    {
+        //public int? TenantId { get; set; }
+        //public long UserId { get; set; }
+        //public int RoleId { get; set; }
+        public Guid? IdChiNhanh { get; set; }
+    }
+}
