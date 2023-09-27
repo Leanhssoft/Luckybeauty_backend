b2fd748cf7f296c18e0a0db56002c580e13eb89d
NhuongDT <inftech.ts91@gmail.com>
2023-09-27 10:06:24 +0700

    class GetAllQuyHoaDonItemDto: add props (SDTNguoiNop, NoiDungThu)

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/Quy/DM_QuyHoaDon/Dto/GetAllQuyHoaDonItemDto.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/Quy/DM_QuyHoaDon/Dto/GetAllQuyHoaDonItemDto.cs
@@ -6,17 +6,19 @@ namespace BanHangBeautify.Quy.DM_QuyHoaDon.Dto
     {
         public Guid Id { set; get; }
         public Guid? IdChiNhanh { set; get; }
-        public int? IdLoaiChungTu { set; get; } = 11;// used to check print many
+        public int? IdLoaiChungTu { set; get; }// used to check print many
         public Guid? IdHoaDonLienQuan { set; get; } // get để check bên ngoài giao diện (phiếu nào thu/chi từ hóa đơn)
         public string LoaiPhieu { get; set; }// 11.thu/12.chi
         public string MaHoaDon { get; set; }
         public DateTime? NgayLapHoaDon { get; set; }
         public double TongTienThu { get; set; }
         public string TenNguoiNop { get; set; }
+        public string SDTNguoiNop { get; set; }
         public DateTime CreationTime { get; set; }
         public string TenKhoanThuChi { get; set; }
         public string HinhThucThanhToan { get; set; }//1,2,3
         public string SHinhThucThanhToan { get; set; }// mat, pos, ck
+        public string NoiDungThu { get; set; }// mat, pos, ck
         public string TxtTrangThai { get; set; }
         public int? TrangThai { get; set; }
     }
