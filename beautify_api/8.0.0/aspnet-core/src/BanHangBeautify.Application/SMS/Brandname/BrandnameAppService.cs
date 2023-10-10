b6163971dcb3d9a60fc02721f4e736f4726a2de9
NhuongDT <inftech.ts91@gmail.com>
2023-10-10 08:24:27 +0700

    BrandnameAppService: add some func: CheckexistSDT, ExportToExcel_ListBrandname, DeleteMultiple_Brandname

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/SMS/Brandname/BrandnameAppService.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/SMS/Brandname/BrandnameAppService.cs
@@ -1,11 +1,13 @@
 ﻿using Abp.Application.Services.Dto;
 using Abp.Domain.Repositories;
 using BanHangBeautify.Data.Entities;
+using BanHangBeautify.DataExporting.Excel.EpPlus;
 using BanHangBeautify.Entities;
 using BanHangBeautify.HangHoa.HangHoa.Repository;
 using BanHangBeautify.HangHoa.NhomHangHoa.Dto;
 using BanHangBeautify.SMS.Brandname.Repository;
 using BanHangBeautify.SMS.Dto;
+using BanHangBeautify.Storage;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -19,11 +21,36 @@ namespace BanHangBeautify.SMS.Brandname
     {
         private readonly IRepository<HT_SMSBrandname, Guid> _dmBrandname;
         private readonly IBrandnameRepository _repository;
+        private readonly IExcelBase _excelBase;
 
-        public BrandnameAppService(IRepository<HT_SMSBrandname, Guid> dmBrandname, IBrandnameRepository repository)
+        public BrandnameAppService(IRepository<HT_SMSBrandname, Guid> dmBrandname, IBrandnameRepository repository,
+            IExcelBase excelBase)
         {
             _dmBrandname = dmBrandname;
             _repository = repository;
+            _excelBase = excelBase;
+        }
+
+        [HttpGet]
+        public async Task<bool> Brandname_CheckExistSDT(string phoneNumber, Guid? id = null)
+        {
+            if (id != null && id != Guid.Empty)
+            {
+                var lst = await _dmBrandname.GetAllListAsync(x => x.Id != id && x.SDTCuaHang == phoneNumber.Trim());
+                if (lst.Count > 0)
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                var lst = await _dmBrandname.GetAllListAsync(x => x.SDTCuaHang == phoneNumber.Trim());
+                if (lst.Count > 0)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
         [HttpGet]
         public async Task<PageBrandnameDto> GetInforBrandnamebyID(Guid id)
@@ -96,6 +123,45 @@ namespace BanHangBeautify.SMS.Brandname
             {
                 return string.Concat(ex.InnerException + ex.Message);
             }
+        }
+        [HttpPost]
+        public async Task DeleteMultiple_Brandname(List<Guid> lstIdBrandname)
+        {
+            _dmBrandname.GetAllList(x => lstIdBrandname.Contains(x.Id)).ToList().ForEach(x =>
+            {
+                x.TrangThai = 0;
+                x.IsDeleted = true;
+                x.DeletionTime = DateTime.Now;
+                x.DeleterUserId = AbpSession.UserId;
+            });
+        }
+        [HttpPost]
+        public async Task ActiveMultiple_Brandname(List<Guid> lstIdBrandname)
+        {
+            _dmBrandname.GetAllList(x => lstIdBrandname.Contains(x.Id)).ToList().ForEach(x =>
+            {
+                x.TrangThai = 1;
+                x.IsDeleted = false;
+                x.LastModificationTime = DateTime.Now;
+                x.LastModifierUserId = AbpSession.UserId;
+            });
+        }
+
+        [HttpPost]
+        public async Task<FileDto> ExportToExcel_ListBrandname(PagedRequestDto input)
+        {
+            var data = await _repository.GetListBandname(input, AbpSession.TenantId ?? 1);
+            var dataExcel = ObjectMapper.Map<List<PageBrandnameDto>>(data.Items);
+            var dataNew = dataExcel.Select(x => new
+            {
+                x.Brandname,
+                x.SDTCuaHang,
+                x.NgayKichHoat,
+                x.TongTienNap,
+                x.DaSuDung,
+                x.ConLai
+            }).ToList();
+            return _excelBase.WriteToExcel("DanhSachBrandname_", "Brandname_Export_Template.xlsx", dataNew, 5);
         }
     }
 }
