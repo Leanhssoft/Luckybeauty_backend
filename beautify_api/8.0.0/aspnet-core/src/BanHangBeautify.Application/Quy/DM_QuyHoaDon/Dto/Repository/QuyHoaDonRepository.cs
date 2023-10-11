e4b23a7d1c017d13776078682b9ed0dd9b07d5b0
NhuongDT <inftech.ts91@gmail.com>
2023-10-11 19:33:43 +0700

    QuyHoaDonRepository: change (prc_SoQuy_GetAll -->spGetAllSoQuy)

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/Quy/DM_QuyHoaDon/Dto/Repository/QuyHoaDonRepository.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/Quy/DM_QuyHoaDon/Dto/Repository/QuyHoaDonRepository.cs
@@ -31,40 +31,38 @@ namespace BanHangBeautify.Quy.DM_QuyHoaDon.Dto.Repository
         }
         public async Task<PagedResultDto<GetAllQuyHoaDonItemDto>> Search(PagedQuyHoaDonRequestDto input)
         {
-            using (var command = CreateCommand("prc_SoQuy_GetAll"))
+            using var command = CreateCommand("spGetAllSoQuy");
+            string idChiNhanhs = string.Empty;
+            if (input.IdChiNhanhs != null && input.IdChiNhanhs.Count > 0)
             {
-                string idChiNhanhs = string.Empty;
-                if (input.IdChiNhanhs != null && input.IdChiNhanhs.Count > 0)
-                {
-                    idChiNhanhs = string.Join(",", input.IdChiNhanhs);
-                }
-                command.Parameters.Add(new SqlParameter("@TenantId", input.TenantId ?? 1));
-                command.Parameters.Add(new SqlParameter("@Filter", input.TextSearch ?? ""));
-                command.Parameters.Add(new SqlParameter("@IdChiNhanh", idChiNhanhs));
-                command.Parameters.Add(new SqlParameter("@FromDate", input.FromDate ?? (object)DBNull.Value));
-                command.Parameters.Add(new SqlParameter("@ToDate", input.ToDate ?? (object)DBNull.Value));
-                command.Parameters.Add(new SqlParameter("@SortBy", input.ColumnSort ?? "ngayLapHoaDon"));
-                command.Parameters.Add(new SqlParameter("@SortType", input.TypeSort ?? "desc"));
-                command.Parameters.Add(new SqlParameter("@MaxResultCount", input.PageSize));
-                command.Parameters.Add(new SqlParameter("@SkipCount", input.CurrentPage));
-
-                using (var dataReader = await command.ExecuteReaderAsync())
-                {
-                    string[] array = { "Data" };
-                    var ds = new DataSet();
-                    ds.Load(dataReader, LoadOption.OverwriteChanges, array);
-                    if (ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
-                    {
-                        var data = ObjectHelper.FillCollection<GetAllQuyHoaDonItemDto>(ds.Tables[0]);
-                        return new PagedResultDto<GetAllQuyHoaDonItemDto>()
-                        {
-                            TotalCount = int.Parse(ds.Tables[0].Rows[0]["TotalCount"].ToString()),
-                            Items = data
-                        };
-                    }
-                }
-                return new PagedResultDto<GetAllQuyHoaDonItemDto>();
+                idChiNhanhs = string.Join(",", input.IdChiNhanhs);
             }
+            command.Parameters.Add(new SqlParameter("@TenantId", input.TenantId ?? 1));
+            command.Parameters.Add(new SqlParameter("@Filter", input.TextSearch ?? ""));
+            command.Parameters.Add(new SqlParameter("@IdChiNhanh", idChiNhanhs));
+            command.Parameters.Add(new SqlParameter("@FromDate", input.FromDate ?? (object)DBNull.Value));
+            command.Parameters.Add(new SqlParameter("@ToDate", input.ToDate ?? (object)DBNull.Value));
+            command.Parameters.Add(new SqlParameter("@SortBy", input.ColumnSort ?? "ngayLapHoaDon"));
+            command.Parameters.Add(new SqlParameter("@SortType", input.TypeSort ?? "desc"));
+            command.Parameters.Add(new SqlParameter("@MaxResultCount", input.PageSize));
+            command.Parameters.Add(new SqlParameter("@SkipCount", input.CurrentPage));
+
+            using (var dataReader = await command.ExecuteReaderAsync())
+            {
+                string[] array = { "Data" };
+                var ds = new DataSet();
+                ds.Load(dataReader, LoadOption.OverwriteChanges, array);
+                if (ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+                {
+                    var data = ObjectHelper.FillCollection<GetAllQuyHoaDonItemDto>(ds.Tables[0]);
+                    return new PagedResultDto<GetAllQuyHoaDonItemDto>()
+                    {
+                        TotalCount = int.Parse(ds.Tables[0].Rows[0]["TotalCount"].ToString()),
+                        Items = data
+                    };
+                }
+            }
+            return new PagedResultDto<GetAllQuyHoaDonItemDto>();
         }
         public async Task<List<QuyHoaDonViewItemDto>> GetNhatKyThanhToan_ofHoaDon(Guid idHoaDonLienQuan)
         {
