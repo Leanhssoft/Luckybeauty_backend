e4325c22fd51ae6041d4615f2c3d2a60d7e98575
NhuongDT <inftech.ts91@gmail.com>
2023-10-11 19:46:51 +0700

    add table SMS_LichSuNap_ChuyenTien & add-migration

---
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.EntityFrameworkCore/SP_Migrations/20231011095348_AddEntitiesSMS_LichSuNap_ChuyenTien.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BanHangBeautify.SPMigrations
+{
+    /// <inheritdoc />
+    public partial class AddEntitiesSMSLichSuNapChuyenTien : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SMS_LichSuNap_ChuyenTien",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    TenantId = table.Column<int>(type: "int", nullable: false),
+                    IdPhieuNapTien = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    ThoiGianNapChuyenTien = table.Column<DateTime>(name: "ThoiGianNap_ChuyenTien", type: "datetime2", nullable: false),
+                    IdNguoiChuyenTien = table.Column<long>(type: "bigint", nullable: true),
+                    IdNguoiNhanTien = table.Column<long>(type: "bigint", nullable: true),
+                    SoTienChuyenNhan = table.Column<double>(name: "SoTienChuyen_Nhan", type: "float", nullable: true),
+                    NoiDungChuyenNhan = table.Column<string>(name: "NoiDungChuyen_Nhan", type: "nvarchar(max)", nullable: true),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatorUserId = table.Column<long>(type: "bigint", nullable: true),
+                    LastModificationTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifierUserId = table.Column<long>(type: "bigint", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    DeleterUserId = table.Column<long>(type: "bigint", nullable: true),
+                    DeletionTime = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SMS_LichSuNap_ChuyenTien", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SMS_LichSuNap_ChuyenTien");
+        }
+    }
+}
