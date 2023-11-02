f51834afe9992d76c0326fb6df2364ffc8ab0226
NhuongDT <inftech.ts91@gmail.com>
2023-11-02 20:46:10 +0700

    add  TypeFilterDateConst

---
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/Configuration/Common/Consts/TypeFilterDate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanHangBeautify.Configuration.Common.Consts
+{
+    public class TypeFilterDateConst
+    {
+        public const string HOM_NAY= "HOM_NAY";
+        public const string HOM_QUA = "HOM_QUA";
+        public const string NGAY_MAI = "NGAY_MAI";
+        public const string TUAN_NAY = "TUAN_NAY";
+        public const string TUAN_TRUOC = "TUAN_TRUOC";
+        public const string TUAN_TOI = "TUAN_TOI";
+        public const string THANG_NAY = "THANG_NAY";
+        public const string THANG_TRUOC = "THANG_TRUOC";
+        public const string THANG_TOI = "THANG_TOI";
+        public const string QUY_NAY = "QUY_NAY";
+        public const string QUY_TRUOC= "QUY_TRUOC";
+        public const string NAM_NAY= "NAM_NAY";
+        public const string NAM_TRUOC= "NAM_TRUOC";
+        public const string TUY_CHON= "TUY_CHON";
+        public const string TAT_CA= "TAT_CA";
+    }
+}
