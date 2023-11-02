19bcb93ed40c150772bd3f648f287f8e3300e7b8
NhuongDT <inftech.ts91@gmail.com>
2023-11-02 20:49:47 +0700

    CreateOrEditHeThongSMSDto

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/SMS/Dto/CreateOrEditHeThongSMSDto.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/SMS/Dto/CreateOrEditHeThongSMSDto.cs
@@ -5,17 +5,21 @@ namespace BanHangBeautify.SMS.Dto
     public class CreateOrEditHeThongSMSDto
     {
         public Guid Id { set; get; }
-        public Guid IdChiNhanh { set; get; }
-        public Guid IdNguoiGui { set; get; }
-        public Guid IdKhachHang { set; get; }
-        public Guid IdHoaDon { set; get; }
-        public Guid IdTinNhan { set; get; }
+        public Guid? IdChiNhanh { set; get; } = null;
+        public Guid? IdNguoiGui { set; get; }
+        public Guid? IdKhachHang { set; get; }
+        public Guid? IdHoaDon { set; get; }
+        public Guid? IdTinNhan { set; get; }
         public string SoDienThoai { set; get; }
-        public int SoTinGui { set; get; }
+        public int? SoTinGui { set; get; } = 1;
         public string NoiDungTin { set; get; }
-        public DateTime ThoiGianGui { set; get; }
-        public byte LoaiTin { set; get; }
-        public double PhiGuiTin { get; set; }
-        public int TrangThai { get; set; }
+        public DateTime? ThoiGianGui { set; get; } = DateTime.Now;
+        public byte? IdLoaiTin { set; get; } = 1;
+        public double? GiaTienMoiTinNhan { get; set; } = 1000;
+        public int? TrangThai { get; set; } = 100;
+
+        public string STrangThaiGuiTinNhan { set; get; }
+        public string LoaiTin { set; get; }
+        public string TenKhachHang { set; get; }
     }
 }
