8b202ac187b80dd4b9c7992a70c05b36c702c1fc
NhuongDT <inftech.ts91@gmail.com>
2023-11-03 20:28:45 +0700

    add class CustomerBasicDto

---
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/KhachHang/KhachHang/Dto/CustomerBasicDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanHangBeautify.KhachHang.KhachHang.Dto
+{
+    public class CustomerBasicDto
+    {
+        public Guid Id { get; set; }
+        public Guid IdKhachHang { get { return Id; } }
+        public string MaKhachHang { get; set; }
+        public string TenKhachHang { get; set; }
+        public string SoDienThoai { get; set; }
+    }
+}
