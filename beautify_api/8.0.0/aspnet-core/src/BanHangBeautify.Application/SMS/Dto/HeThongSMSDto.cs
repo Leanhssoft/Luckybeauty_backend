b00be9dbf442329b2c02f328c49031aa803b45ac
NhuongDT <inftech.ts91@gmail.com>
2023-11-03 20:29:43 +0700

    remove class HeThongSMSDto
    add class PageKhachHangSMSDto

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/SMS/Dto/HeThongSMSDto.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/SMS/Dto/HeThongSMSDto.cs
@@ -1,21 +1,16 @@
-﻿using System;
+﻿using BanHangBeautify.KhachHang.KhachHang.Dto;
+using System;
 
 namespace BanHangBeautify.SMS.Dto
 {
-    public class HeThongSMSDto
+    public class PageKhachHangSMSDto : CustomerBasicDto
     {
-        public Guid Id { set; get; }
-        public Guid IdChiNhanh { set; get; }
-        public Guid IdNguoiGui { set; get; }
-        public Guid IdKhachHang { set; get; }
-        public Guid IdHoaDon { set; get; }
-        public Guid IdTinNhan { set; get; }
-        public string SoDienThoai { set; get; }
-        public int SoTinGui { set; get; }
-        public string NoiDungTin { set; get; }
-        public DateTime ThoiGianGui { set; get; }
-        public byte LoaiTin { set; get; }
-        public double PhiGuiTin { get; set; }
-        public int TrangThai { get; set; }
+        public DateTime? NgaySinh { set; get; } = null;
+        public string MaHoaDon { get; set; }
+        public DateTime? NgayLapHoaDon { set; get; } = null;
+        public string TenHangHoa { get; set; }
+        public DateTime? BookingDate { set; get; } = null;
+        public string ThoiGianHen { set; get; } // từ HH:mm - HH:mm
+        public string STrangThaiGuiTinNhan { get; set; }
     }
 }
