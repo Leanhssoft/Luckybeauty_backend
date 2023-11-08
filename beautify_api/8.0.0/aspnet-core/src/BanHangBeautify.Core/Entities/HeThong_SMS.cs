beed050c6965fecd3f1ecc7df3c3754227d68179
NhuongDT <inftech.ts91@gmail.com>
2023-11-08 16:16:28 +0700

    IdNguoiGui: Guid --> long (FK to User)UserId
    IdTinNhan: Guid --> string

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Core/Entities/HeThong_SMS.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Core/Entities/HeThong_SMS.cs
@@ -1,5 +1,7 @@
-﻿using Abp.Domain.Entities;
+﻿using Abp.Authorization.Users;
+using Abp.Domain.Entities;
 using Abp.Domain.Entities.Auditing;
+using BanHangBeautify.Authorization.Users;
 using BanHangBeautify.Data.Entities;
 using System;
 using System.ComponentModel.DataAnnotations;
@@ -10,9 +12,9 @@ namespace BanHangBeautify.Entities
     public class HeThong_SMS : FullAuditedEntity<Guid>, IMustHaveTenant
     {
         public int TenantId { get; set; }
-        public Guid? IdNguoiGui { set; get; }// cửa hàng tự quản lý số tiền tối đa cho phép NV được gửi
+        public long? IdNguoiGui { set; get; }// cửa hàng tự quản lý số tiền tối đa cho phép NV được gửi
         [ForeignKey(nameof(IdNguoiGui))]
-        public NS_NhanVien NS_NhanVien { get; set; }
+        public User User { get; set; }
         public Guid IdChiNhanh { set; get; }
         [ForeignKey(nameof(IdChiNhanh))]
         public DM_ChiNhanh DM_ChiNhanh { get; set; }
@@ -22,7 +24,7 @@ namespace BanHangBeautify.Entities
         public Guid? IdHoaDon { set; get; }
         [ForeignKey(nameof(IdHoaDon))]
         public BH_HoaDon BH_HoaDon { get; set; }
-        public Guid? IdTinNhan { set; get; }// dc trả về = qua ESMS
+        public string IdTinNhan { set; get; }// dc trả về = ESMS API (vd: a4e3472e-5ff8-4a03-beb6-38b3393a42b5186 (gần giống Guid nhưng không phải - vì chuỗi cuối cùng gồm 15 kí tự))
         public string SoDienThoai { set; get; }
         public int SoTinGui { set; get; }
         [MaxLength(4000)]
