b386362c5b3516caccbf8cd709d96be50ac3b8bb
NhuongDT <inftech.ts91@gmail.com>
2023-11-08 16:10:57 +0700

    add-migration: (IdNguoiGui - HeThongSMS)

---
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.EntityFrameworkCore/SP_Migrations/20231106102622_Add_Column_IdNguoiGui_FK_toAbpUser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BanHangBeautify.SPMigrations
+{
+    /// <inheritdoc />
+    public partial class AddColumnIdNguoiGuiFKtoAbpUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "IdNguoiGui",
+                table: "HeThong_SMS",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HeThong_SMS_IdNguoiGui",
+                table: "HeThong_SMS",
+                column: "IdNguoiGui");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HeThong_SMS_AbpUsers_IdNguoiGui",
+                table: "HeThong_SMS",
+                column: "IdNguoiGui",
+                principalTable: "AbpUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_HeThong_SMS_AbpUsers_IdNguoiGui",
+                table: "HeThong_SMS");
+
+            migrationBuilder.DropIndex(
+                name: "IX_HeThong_SMS_IdNguoiGui",
+                table: "HeThong_SMS");
+
+            migrationBuilder.DropColumn(
+                name: "IdNguoiGui",
+                table: "HeThong_SMS");
+        }
+    }
+}
