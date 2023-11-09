ab74054e3ca0835cefe97557920f2247ad5e9676
NhuongDT <inftech.ts91@gmail.com>
2023-11-09 17:17:24 +0700

    add func GetBrandnameBalance_byUserLogin

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/SMS/LichSuNap_ChuyenTien/LichSuNap_ChuyenTienAppService.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/SMS/LichSuNap_ChuyenTien/LichSuNap_ChuyenTienAppService.cs
@@ -5,6 +5,8 @@ using BanHangBeautify.DatLichOnline.Dto;
 using BanHangBeautify.Entities;
 using BanHangBeautify.HangHoa.HangHoa.Dto;
 using BanHangBeautify.SMS.Dto;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -18,13 +20,16 @@ namespace BanHangBeautify.SMS.LichSuNap_ChuyenTien
     {
         private readonly IUnitOfWorkManager _unitOfWorkManager;
         private readonly IRepository<SMS_LichSuNap_ChuyenTien, Guid> _lichSuNapTien;
+        private readonly IRepository<HeThong_SMS, Guid> _hethongSMS;
 
-        public LichSuNap_ChuyenTienAppService(IUnitOfWorkManager unitOfWorkManager, IRepository<SMS_LichSuNap_ChuyenTien, Guid> lichSuNapTien)
+        public LichSuNap_ChuyenTienAppService(IUnitOfWorkManager unitOfWorkManager, IRepository<SMS_LichSuNap_ChuyenTien, Guid> lichSuNapTien, IRepository<HeThong_SMS, Guid> hethongSMS)
         {
             _unitOfWorkManager = unitOfWorkManager;
             _lichSuNapTien = lichSuNapTien;
+            _hethongSMS = hethongSMS;
         }
 
+        [HttpPost]
         public async Task<LichSuNap_ChuyenTienDto> ThemMoiPhieuNapTien(int tenantId, LichSuNap_ChuyenTienDto input)
         {
             var tenant = await TenantManager.FindByIdAsync(tenantId);
@@ -50,6 +55,7 @@ namespace BanHangBeautify.SMS.LichSuNap_ChuyenTien
             }
         }
 
+        [HttpPost]
         public async Task<LichSuNap_ChuyenTienDto> ThemMoi_CapNhatPhieuNapTien(int tenantId, LichSuNap_ChuyenTienDto input)
         {
             var tenant = await TenantManager.Tenants.FirstOrDefaultAsync(x => x.Id == tenantId);
@@ -82,6 +88,32 @@ namespace BanHangBeautify.SMS.LichSuNap_ChuyenTien
             else
             {
                 return null;
+            }
+        }
+
+        [HttpGet]
+        public async Task<double?> GetBrandnameBalance_byUserLogin(long userId)
+        {
+            try
+            {
+                // tongnap theo user
+                var lstNapTien = await _lichSuNapTien.GetAllListAsync(x => x.IdNguoiNhanTien == userId);
+                double? tongNap = lstNapTien.Sum(x => x.SoTienChuyen_Nhan);
+
+                // tonggui
+                var lstGuiTien_otherUser = await _lichSuNapTien.GetAllListAsync(x => x.IdNguoiChuyenTien == userId);
+                double? tongGui = lstNapTien.Sum(x => x.SoTienChuyen_Nhan);
+
+                // tonggui SMS thanh cong
+                var lstGuiTinSMS = await _hethongSMS.GetAllListAsync(x => x.IdNguoiGui == userId && x.TrangThai == 100);
+                double? tongGuiSMS = lstGuiTinSMS.Sum(x => x.SoTinGui * x.GiaTienMoiTinNhan);
+
+                double? sodu = tongNap ?? 0 - tongGui ?? 0 - tongGuiSMS ?? 0;
+                return sodu;
+            }
+            catch (Exception)
+            {
+                return 0;
             }
         }
     }
