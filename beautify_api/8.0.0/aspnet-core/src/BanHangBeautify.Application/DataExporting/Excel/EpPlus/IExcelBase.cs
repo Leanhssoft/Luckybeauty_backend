e12ae53a932e1d178b5123bd915dcf4898d2aa9d
NhuongDT <inftech.ts91@gmail.com>
2023-11-09 17:15:17 +0700

    WriteToExcel: pass  List<Excel_CellData>

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/DataExporting/Excel/EpPlus/IExcelBase.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/DataExporting/Excel/EpPlus/IExcelBase.cs
@@ -1,10 +1,11 @@
 ﻿using BanHangBeautify.Storage;
 using System.Collections.Generic;
+using static BanHangBeautify.Configuration.Common.CommonClass;
 
 namespace BanHangBeautify.DataExporting.Excel.EpPlus
 {
     public interface IExcelBase
     {
-        FileDto WriteToExcel<T>(string fileName, string fileTemplate, List<T> listData, int startRow = 5);
+        FileDto WriteToExcel<T>(string fileName, string fileTemplate, List<T> listData, int startRow = 5, List<Excel_CellData> param = null);
     }
 }
