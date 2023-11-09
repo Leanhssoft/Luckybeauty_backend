dc74b3176349e3f8178d9c69d341c454af183d2b
NhuongDT <inftech.ts91@gmail.com>
2023-11-09 17:16:39 +0700

    add ExportToExcel_DanhSachTinNhan, DanhSachKhachHang_SMS

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/SMS/GuiTinNhan/HeThongSMSAppService.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/SMS/GuiTinNhan/HeThongSMSAppService.cs
@@ -15,6 +15,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using BanHangBeautify.SMS.GuiTinNhan.Repository;
 using static BanHangBeautify.Configuration.Common.CommonClass;
+using BanHangBeautify.Storage;
+using BanHangBeautify.DataExporting.Excel.EpPlus;
+using System.Globalization;
 
 namespace BanHangBeautify.SMS.GuiTinNhan
 {
@@ -23,11 +26,13 @@ namespace BanHangBeautify.SMS.GuiTinNhan
     {
         private readonly IRepository<HeThong_SMS, Guid> _hethongSMS;
         public readonly IHeThongSMSRepository _repoSMS;
+        private readonly IExcelBase _excelBase;
 
-        public HeThongSMSAppService(IRepository<HeThong_SMS, Guid> repository, IHeThongSMSRepository repoSMS)
+        public HeThongSMSAppService(IRepository<HeThong_SMS, Guid> repository, IHeThongSMSRepository repoSMS, IExcelBase excelBase)
         {
             _hethongSMS = repository;
             _repoSMS = repoSMS;
+            _excelBase = excelBase;
         }
         [HttpPost]
         public async Task<CreateOrEditHeThongSMSDto> Insert_HeThongSMS(CreateOrEditHeThongSMSDto input)
@@ -103,5 +108,115 @@ namespace BanHangBeautify.SMS.GuiTinNhan
             var data = await _repoSMS.InsertNhatKyGuiTinSMS(input, AbpSession.TenantId ?? 1);
             return data > 0; ;
         }
+
+        #region Export Excel
+        [HttpPost]
+        public async Task<FileDto> ExportToExcel_DanhSachTinNhan(ParamSearch input)
+        {
+            var data = await _repoSMS.GetListSMS(input);
+            var dataExcel = ObjectMapper.Map<List<CreateOrEditHeThongSMSDto>>(data.Items);
+            var dataNew = dataExcel.Select(x => new
+            {
+                x.ThoiGianGui,
+                x.TenKhachHang,
+                x.SoDienThoai,
+                x.LoaiTin,
+                x.NoiDungTin,
+            }).ToList();
+
+            string fileName = string.Empty, fileTitle = string.Empty;
+            if (input.TrangThais != null && input.TrangThais.Count > 0)
+            {
+                var sTrangThai = String.Join(",", input.TrangThais);
+                switch (sTrangThai)
+                {
+                    case "1":
+                        fileName = "SMSTinNhap_";
+                        fileTitle = "DANH SÁCH TIN NHẮN LƯU NHÁP";
+                        break;
+                    case "100":
+                        fileName = "SMSDaGui_";
+                        fileTitle = "DANH SÁCH TIN NHẮN ĐÃ GỬI";
+                        break;
+                    default:
+                        fileName = "SMSKhongThanhCong_";
+                        fileTitle = "DANH SÁCH TIN NHẮN GỬI KHÔNG THÀNH CÔNG";
+                        break;
+
+                }
+            }
+
+            List<Excel_CellData> lst = new()
+            {
+                new Excel_CellData { RowIndex = 1, ColumnIndex = 1, CellValue = fileTitle }
+            };
+            return _excelBase.WriteToExcel(fileName, @"SMS\SMS_DanhSachTinNhan_Template.xlsx", dataNew, 5, lst);
+        }
+
+        [HttpPost]
+        public async Task<FileDto> ExportToExcel_DanhSachKhachHang_SMS(ParamSearch input, int? idLoaiTin = 1)
+        {
+            var data = await _repoSMS.GetListCustomer_byIdLoaiTin(input, idLoaiTin);
+            var dataExcel = ObjectMapper.Map<List<PageKhachHangSMSDto>>(data.Items);
+
+            FileDto fileReturn = new FileDto();
+            string fileName = string.Empty, dateFromTo = string.Empty;
+            if (input.FromDate != null && input.ToDate != null)
+            {
+                dateFromTo = $"Thời gian: {input.FromDate?.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture)}" +
+                    $" - {input.ToDate?.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture)}";
+            }
+            List<Excel_CellData> lst = new();
+            switch (idLoaiTin)
+            {
+                case 2:// sinhnhat 
+                    {
+                        fileName = "DSKhachHang_SinhNhat_";
+                        var dataNew = dataExcel.Select(x => new
+                        {
+                            x.TenKhachHang,
+                            x.SoDienThoai,
+                            x.NgaySinh,
+                            x.STrangThaiGuiTinNhan,
+                        }).ToList();
+                        lst.Add(new Excel_CellData { RowIndex = 2, ColumnIndex = 1, CellValue = dateFromTo });
+                        fileReturn = _excelBase.WriteToExcel(fileName, "SMS_KhachHangSinhNhat_Template.xlsx", dataNew, 5, lst);
+                    }
+                    break;
+                case 3:
+                    {
+                        fileName = "DSKhachHang_LichHen_";
+                        var dataNew = dataExcel.Select(x => new
+                        {
+                            x.TenKhachHang,
+                            x.SoDienThoai,
+                            x.BookingDate,
+                            x.ThoiGianHen,
+                            x.TenHangHoa,
+                            x.STrangThaiGuiTinNhan,
+                        }).ToList();
+                        lst.Add(new Excel_CellData { RowIndex = 2, ColumnIndex = 1, CellValue = dateFromTo });
+                        fileReturn = _excelBase.WriteToExcel(fileName, "SMS_KhachHangLichHen_Template.xlsx", dataNew, 5, lst);
+                    }
+                    break;
+                case 4:
+                    {
+                        fileName = "DSKhachHang_GiaoDicH_";
+                        var dataNew = dataExcel.Select(x => new
+                        {
+                            x.TenKhachHang,
+                            x.SoDienThoai,
+                            x.MaHoaDon,
+                            x.NgayLapHoaDon,
+                            x.STrangThaiGuiTinNhan,
+                        }).ToList();
+                        lst.Add(new Excel_CellData { RowIndex = 2, ColumnIndex = 1, CellValue = dateFromTo });
+                        fileReturn = _excelBase.WriteToExcel(fileName, "SMS_KhachHangGiaoDich_Template.xlsx", dataNew, 5, lst);
+                    }
+                    break;
+            }
+            return fileReturn;
+        }
+        #endregion
     }
 }
