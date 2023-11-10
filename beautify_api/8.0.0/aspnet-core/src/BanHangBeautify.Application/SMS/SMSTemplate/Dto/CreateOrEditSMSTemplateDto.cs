48ef8b79a29f6d58963141ec041fe7ed61e54f8d
manhybt21 <luongmanh3222@gmail.com>
2023-11-10 08:56:23 +0700

    commit

---
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/SMS/SMSTemplate/Dto/CreateOrEditSMSTemplateDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanHangBeautify.SMS.SMSTemplate.Dto
+{
+    public class CreateOrEditSMSTemplateDto
+    {
+        public Guid Id { get; set; }
+        public byte? IdLoaiTin { get; set; }
+        public string TenMauTin { get; set; }
+        public string NoiDungTinMau { get; set; }
+        public bool? LaMacDinh { get; set; }
+        public byte? TrangThai { get; set; }
+    }
+    public class SMSTemplateViewDto
+    {
+        public Guid Id { get; set; }
+        public int IdLoaiTin { get; set; }
+        public string TenMauTin { get; set; }
+        public string NoiDungTinMau { get; set; }
+    }
+}
