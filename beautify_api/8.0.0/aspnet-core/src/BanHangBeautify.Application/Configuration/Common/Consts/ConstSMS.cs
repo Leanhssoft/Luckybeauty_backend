f1dd51d3b325e9e9f4f3093a8c91ebb4f34e6709
NhuongDT <inftech.ts91@gmail.com>
2023-11-11 16:31:47 +0700

    add ConstSMS. LoaiTin

---
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/Configuration/Common/Consts/ConstSMS.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanHangBeautify.Configuration.Common.Consts
+{
+    public class ConstSMS
+    {
+        public class LoaiTin
+        {
+            public const byte TinThuong = 1;
+            public const byte SinhNhat = 2;
+            public const byte LichHen = 3;
+            public const byte GiaoDich = 4;
+        }
+    }
+}
