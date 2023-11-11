6aafb3f599f39fe5547d7a9943b226859defe44e
NhuongDT <inftech.ts91@gmail.com>
2023-11-11 16:32:33 +0700

    add  class EmailDto

---
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/SMS/Dto/EmailDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanHangBeautify.SMS.Dto
+{
+    public class EmailDto
+    {
+        public string TieuDeEmail { get; set; }
+        public string NoiDungEmail { get; set; }
+    }
+}
