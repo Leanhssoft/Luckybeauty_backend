7d76a73ec48fce5c893d624896e68545d840d31d
NhuongDT <inftech.ts91@gmail.com>
2023-11-17 13:52:22 +0700

    rewrite code + check Brand name (by TenantId)

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/BackgroundWorker/SendEmailSMSAutoWorker.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/BackgroundWorker/SendEmailSMSAutoWorker.cs
@@ -16,6 +16,11 @@ using BanHangBeautify.Configuration.Common;
 using BanHangBeautify.Configuration.Common.Consts;
 using BanHangBeautify.MultiTenancy;
 using BanHangBeautify.Authorization.Users;
+using BanHangBeautify.SMS.Brandname.Repository;
+using Abp.Net.Mail;
+using BanHangBeautify.Configuration.Dto;
+using Abp.Configuration;
+using NPOI.XWPF.UserModel;
 
 namespace BanHangBeautify.BackgroundWorker
 {
@@ -23,6 +28,7 @@ namespace BanHangBeautify.BackgroundWorker
     {
         public readonly IESMS _eSMS;
         public readonly IRepository<SMS_CaiDat_NhacNho, Guid> _caiDatNhacNho;
+        public readonly IRepository<CaiDat_NhacNho_ChiTiet, Guid> _caiDatNhacNhoChiTiet;
         public readonly IRepository<DM_ChiNhanh, Guid> _dmChiNhanh;
         public readonly IRepository<HeThong_SMS, Guid> _hethongSMS;
         public readonly IRepository<SMS_NhatKy_GuiTin, Guid> _smsNhatKyGuiTin;
@@ -30,23 +36,31 @@ namespace BanHangBeautify.BackgroundWorker
         private readonly IRepository<Tenant> _tenantRepository;
         public readonly IHeThongSMSRepository _repoSMS;
         private readonly IUnitOfWorkManager _unitOfWorkManager;
+        private readonly SettingManager _settingManager;
+        private readonly IEmailSender _emailSender;
+        private readonly IBrandnameRepository _repoBrandname;
         readonly DateTime _dtNow = new(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 0, 0, 0, 0);
 
         public SendEmailSMSAutoWorker(AbpTimer timer,
             IESMS eSMS,
             IRepository<SMS_CaiDat_NhacNho, Guid> caiDatNhacNho,
+            IRepository<CaiDat_NhacNho_ChiTiet, Guid> caiDatNhacNhoChiTiet,
             IRepository<DM_ChiNhanh, Guid> dmChiNhanh,
            IRepository<HeThong_SMS, Guid> hethongSMS,
              IRepository<SMS_NhatKy_GuiTin, Guid> smsNhatKyGuiTin,
              IRepository<User, long> user,
              IRepository<Tenant> tenantRepository,
             IHeThongSMSRepository repoSMS,
-            IUnitOfWorkManager unitOfWorkManager
+            IUnitOfWorkManager unitOfWorkManager,
+            SettingManager settingManager,
+            IEmailSender emailSender,
+            IBrandnameRepository repoBrandname
             ) : base(timer)
         {
             Timer.Period = 10000;
             _eSMS = eSMS;
             _caiDatNhacNho = caiDatNhacNho;
+            _caiDatNhacNhoChiTiet = caiDatNhacNhoChiTiet;
             _dmChiNhanh = dmChiNhanh;
             _hethongSMS = hethongSMS;
             _smsNhatKyGuiTin = smsNhatKyGuiTin;
@@ -54,65 +68,104 @@ namespace BanHangBeautify.BackgroundWorker
             _tenantRepository = tenantRepository;
             _repoSMS = repoSMS;
             _unitOfWorkManager = unitOfWorkManager;
+            _settingManager = settingManager;
+            _emailSender = emailSender;
+            _repoBrandname = repoBrandname;
         }
 
         protected async override void DoWork()
         {
             using var unitOfWork = _unitOfWorkManager.Begin();
             var tenants = _tenantRepository.GetAllList(t => t.ConnectionString != null && t.ConnectionString != "");
+            var lstBrandname = await _repoBrandname.GetListBandname(new PagedRequestDto { Keyword = string.Empty, SkipCount = 0 }, 1);  // get all brand name at host
+
             for (int i = 0; i < tenants.Count; i++)
             {
                 var tenantId = tenants[i].Id;
 
                 using (_unitOfWorkManager.Current.SetTenantId(tenantId))
                 {
-                    //    // your code
                     await _unitOfWorkManager.WithUnitOfWorkAsync(async () =>
                 {
-                    // get allchinhanh of tenantId
-                    var chiNhanh = _dmChiNhanh.GetAll().Select(x => x.Id.ToString()).ToList();
-                    var userAdmin = _user.GetAllList(x => x.IsAdmin).Select(x => x.Id).FirstOrDefault();
+                    // check has settup
                     var lstSetup = _caiDatNhacNho.GetAllList(x => x.TrangThai == 1 &&
                        (x.IdLoaiTin == ConstSMS.LoaiTin.SinhNhat
                        || x.IdLoaiTin == ConstSMS.LoaiTin.GiaoDich
                        || x.IdLoaiTin == ConstSMS.LoaiTin.LichHen)
-                       ).Select(x => new { x.IdLoaiTin, x.NoiDungTin });
-                    var xx = _unitOfWorkManager.Current.GetTenantId();
+                       ).Select(x => new { x.Id, x.IdLoaiTin, x.NoiDungTin });
 
-                    Guid? idChiNhanhFirst = null;
-                    if (chiNhanh != null && chiNhanh.Count > 0)
-                    {
-                        idChiNhanhFirst = new Guid(chiNhanh.FirstOrDefault().ToString());
-                    }
-
-                    CommonClass.ParamSearch param = new()
-                    {
-                        FromDate = _dtNow,
-                        ToDate = _dtNow,
-                        IdChiNhanhs = chiNhanh,
-                        TrangThais = new List<string> { "0" }, // chi get tin chua gui (trangthai = 0)
-                        CurrentPage = 1,
-                        PageSize = 10000,
-                    };
                     if (lstSetup.Any())
                     {
-                        var lstLoaiTin = lstSetup.Select(x => x.IdLoaiTin);
-                        ////// todo check CaiDat_NhacTuDong_ChiTiet: HinhThucGui(sms / zalo / gmail)
-                        if (lstLoaiTin.Contains(ConstSMS.LoaiTin.SinhNhat))
+                        var chiNhanh = _dmChiNhanh.GetAll().Select(x => x.Id.ToString()).ToList();
+                        var userAdmin = _user.GetAllList(x => x.IsAdmin).Select(x => x.Id).FirstOrDefault();
+                        var lstIdSetup = lstSetup.Select(x => x.Id);
+                        var brandName_ofTenant = lstBrandname.Items.Where(x => x.TenantId == tenantId);
+
+                        InforAutoWorker inforCommon = new()
                         {
-                            string noidungTinMau = lstSetup.Where(x => x.IdLoaiTin == ConstSMS.LoaiTin.SinhNhat).Select(x => x.NoiDungTin)?.FirstOrDefault()?.ToString();
-                            await SaveData(tenantId, idChiNhanhFirst, ConstSMS.LoaiTin.SinhNhat, param, noidungTinMau, userAdmin);
-                        }
-                        if (lstLoaiTin.Contains(ConstSMS.LoaiTin.LichHen))
+                            TenantId = tenantId,
+                            UserId = userAdmin,
+                        };
+                        if (chiNhanh != null && chiNhanh.Count > 0)
                         {
-                            string noidungTinMau = lstSetup.Where(x => x.IdLoaiTin == ConstSMS.LoaiTin.LichHen).Select(x => x.NoiDungTin)?.FirstOrDefault()?.ToString();
-                            await SaveData(tenantId, idChiNhanhFirst, ConstSMS.LoaiTin.LichHen, param, noidungTinMau, userAdmin);
+                            inforCommon.IdChiNhanhFirst = new Guid(chiNhanh.FirstOrDefault().ToString());
                         }
-                        if (lstLoaiTin.Contains(ConstSMS.LoaiTin.GiaoDich))
+
+                        CommonClass.ParamSearch param = new()
                         {
-                            string noidungTinMau = lstSetup.Where(x => x.IdLoaiTin == ConstSMS.LoaiTin.GiaoDich).Select(x => x.NoiDungTin)?.FirstOrDefault()?.ToString();
-                            await SaveData(tenantId, idChiNhanhFirst, ConstSMS.LoaiTin.GiaoDich, param, noidungTinMau, userAdmin);
+                            FromDate = _dtNow,
+                            ToDate = _dtNow,
+                            IdChiNhanhs = chiNhanh,
+                            TrangThais = new List<string> { "0" }, // chi get tin chua gui (trangthai = 0)
+                            CurrentPage = 1,
+                            PageSize = 10000,
+                        };
+
+                        var lstSetupDetail = _caiDatNhacNhoChiTiet.GetAllList(x => lstIdSetup.Contains(x.IdCaiDatNhacNho) && x.TrangThai == 1)
+                        .Select(x => new { x.IdCaiDatNhacNho, x.HinhThucGui });
+
+                        var tblJoin = (from setup in lstSetup
+                                       join setupDetail in lstSetupDetail on setup.Id equals setupDetail.IdCaiDatNhacNho
+                                       select new
+                                       {
+                                           setupDetail.IdCaiDatNhacNho,
+                                           setupDetail.HinhThucGui,
+                                           setup.IdLoaiTin,
+                                           setup.NoiDungTin
+                                       }).ToList();
+
+                        if (brandName_ofTenant.Any())
+                        {
+                            inforCommon.BrandNameFirst = brandName_ofTenant.FirstOrDefault().Brandname;
+
+                            var turnOnSMS = tblJoin.Where(x => x.HinhThucGui == 1).Select(x => x);
+                            foreach (var item in turnOnSMS)
+                            {
+                                if (!string.IsNullOrEmpty(item.NoiDungTin))
+                                {
+                                    inforCommon.NoiDungTinNhan = item.NoiDungTin;
+                                    await SendSMS(item.IdLoaiTin, param, inforCommon);
+                                }
+                            }
                         }
+
+                        var turnOnZalo = tblJoin.Where(x => x.HinhThucGui == 3).Select(x => x);
+                        foreach (var item in turnOnZalo)
+                        {
+                            inforCommon.NoiDungTinNhan = item.NoiDungTin;
+                            await SendSMS(item.IdLoaiTin, param, inforCommon);
+                        }
+
+                        var turnOnEmail = tblJoin.Where(x => x.HinhThucGui == 2).Select(x => x);
+                        foreach (var item in turnOnEmail)
+                        {
+                            if (!string.IsNullOrEmpty(item.NoiDungTin))
+                            {
+                                inforCommon.NoiDungTinNhan = item.NoiDungTin;
+                                await SendEmail(item.IdLoaiTin, param, inforCommon);
+                            }
+                        }
+                        CurrentUnitOfWork.SaveChanges();
                     }
                 });
                 }
@@ -132,23 +185,21 @@ namespace BanHangBeautify.BackgroundWorker
             return ss;
         }
 
-        protected async Task SaveData(int tenantId, Guid? idChiNhanhFirst, byte? idLoaiTin, CommonClass.ParamSearch param, string noidungTinMau, long? userId)
+        protected async Task SendSMS(byte? idLoaiTin, CommonClass.ParamSearch paramSearch, InforAutoWorker inforCommon)
         {
             try
             {
-                var data = await _repoSMS.GetListCustomer_byIdLoaiTin(param, idLoaiTin);
+                var data = await _repoSMS.GetListCustomer_byIdLoaiTin(paramSearch, idLoaiTin);
                 if (data.TotalCount > 0)
                 {
                     var lstCustomer = data.Items;
-
                     foreach (var customer in lstCustomer)
                     {
-                        #region Bitrhday - send SMS
-                        var noidung = ReplaceContent(customer, noidungTinMau);
+                        var noidung = ReplaceContent(customer, inforCommon.NoiDungTinNhan);
                         ESMSDto obj = new()
                         {
                             Phone = customer.SoDienThoai,
-                            Brandname = "brandname",// todo brandname
+                            Brandname = inforCommon.BrandNameFirst,
                             Content = noidung,
                         };
                         // send SMS
@@ -158,9 +209,9 @@ namespace BanHangBeautify.BackgroundWorker
                         HeThong_SMS hethongSMS = new()
                         {
                             Id = Guid.NewGuid(),
-                            TenantId = tenantId,
+                            TenantId = inforCommon.TenantId,
                             IdLoaiTin = idLoaiTin ?? 0,
-                            IdChiNhanh = (Guid)idChiNhanhFirst,
+                            IdChiNhanh = (Guid)inforCommon.IdChiNhanhFirst,
                             IdKhachHang = customer.IdKhachHang,
                             SoDienThoai = customer.SoDienThoai,
                             SoTinGui = (int)Math.Ceiling(noidung.Length / 160.0),
@@ -168,8 +219,8 @@ namespace BanHangBeautify.BackgroundWorker
                             GiaTienMoiTinNhan = 950,
                             TrangThai = smsResult.MessageStatus,
                             NoiDungTin = noidung,
-                            IdNguoiGui = userId,
-                            CreatorUserId = userId,
+                            IdNguoiGui = inforCommon.UserId,
+                            CreatorUserId = inforCommon.UserId,
                             ThoiGianGui = DateTime.Now,
                             CreationTime = DateTime.Now,
                             IsDeleted = false,
@@ -179,23 +230,17 @@ namespace BanHangBeautify.BackgroundWorker
                         SMS_NhatKy_GuiTin nkyGuiSMS = new()
                         {
                             Id = Guid.NewGuid(),
-                            TenantId = tenantId,
+                            TenantId = inforCommon.TenantId,
                             IdHeThongSMS = hethongSMS.Id,
                             ThoiGianTu = _dtNow,
                             ThoiGianDen = _dtNow,
                             IdBooking = idLoaiTin == ConstSMS.LoaiTin.LichHen ? customer.Id : null,
                             IdHoaDon = idLoaiTin == ConstSMS.LoaiTin.GiaoDich ? customer.Id : null,
                             CreationTime = DateTime.Now,
-                            CreatorUserId = userId,
+                            CreatorUserId = inforCommon.UserId,
                             IsDeleted = false,
                         };
                         await _smsNhatKyGuiTin.InsertAsync(nkyGuiSMS);
-                        #endregion
-
-                        #region birthday - send email
-                        // todo
-                        #endregion
-                        CurrentUnitOfWork.SaveChanges();
                     }
                 }
             }
@@ -204,5 +249,80 @@ namespace BanHangBeautify.BackgroundWorker
 
             }
         }
+        protected async Task SendEmail(byte? idLoaiTin, CommonClass.ParamSearch paramSearch, InforAutoWorker inforCommon)
+        {
+            try
+            {
+                var data = await _repoSMS.GetListCustomer_byIdLoaiTin(paramSearch, idLoaiTin);
+                if (data.TotalCount > 0)
+                {
+                    var lstCustomer = data.Items;
+                    foreach (var customer in lstCustomer)
+                    {
+                        int sendStatus = 99;
+                        var noidung = ReplaceContent(customer, inforCommon.NoiDungTinNhan);
+
+                        try
+                        {
+                            await _emailSender.SendAsync(customer.Email, "Test send email", noidung, true);
+                            sendStatus = 100;
+                        }
+                        catch (Exception ex)
+                        {
+
+                        }
+
+                        // save to hethong sms
+                        HeThong_SMS hethongSMS = new()
+                        {
+                            Id = Guid.NewGuid(),
+                            TenantId = inforCommon.TenantId,
+                            IdLoaiTin = idLoaiTin ?? 0,
+                            IdChiNhanh = (Guid)inforCommon.IdChiNhanhFirst,
+                            IdKhachHang = customer.IdKhachHang,
+                            SoDienThoai = customer.SoDienThoai,
+                            SoTinGui = 0,
+                            GiaTienMoiTinNhan = 0,
+                            TrangThai = sendStatus,
+                            NoiDungTin = noidung,
+                            IdNguoiGui = inforCommon.UserId,
+                            CreatorUserId = inforCommon.UserId,
+                            ThoiGianGui = DateTime.Now,
+                            CreationTime = DateTime.Now,
+                            IsDeleted = false,
+                        };
+                        await _hethongSMS.InsertAsync(hethongSMS);
+
+                        SMS_NhatKy_GuiTin nkyGuiSMS = new()
+                        {
+                            Id = Guid.NewGuid(),
+                            TenantId = inforCommon.TenantId,
+                            IdHeThongSMS = hethongSMS.Id,
+                            ThoiGianTu = _dtNow,
+                            ThoiGianDen = _dtNow,
+                            IdBooking = idLoaiTin == ConstSMS.LoaiTin.LichHen ? customer.Id : null,
+                            IdHoaDon = idLoaiTin == ConstSMS.LoaiTin.GiaoDich ? customer.Id : null,
+                            CreationTime = DateTime.Now,
+                            CreatorUserId = inforCommon.UserId,
+                            IsDeleted = false,
+                        };
+                        await _smsNhatKyGuiTin.InsertAsync(nkyGuiSMS);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+
+    public class InforAutoWorker
+    {
+        public int TenantId { get; set; }
+        public long? UserId { get; set; } = 1;
+        public Guid? IdChiNhanhFirst { get; set; }
+        public string BrandNameFirst { get; set; }
+        public string NoiDungTinNhan { get; set; }
     }
 }
