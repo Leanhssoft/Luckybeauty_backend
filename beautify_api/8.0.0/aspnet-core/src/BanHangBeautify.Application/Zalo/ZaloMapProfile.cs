83d3baf58fba825afa149467030b57dc33e8eabd
NhuongDT <inftech.ts91@gmail.com>
2023-12-04 09:56:16 +0700

    add 2 table: ZaloAuthorization, Zalo_KhachHangThanhVien

---
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/Zalo/ZaloMapProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using BanHangBeautify.Entities;
+using BanHangBeautify.HoaDon.HoaDon.Dto;
+using BanHangBeautify.HoaDon.HoaDonChiTiet.Dto;
+using BanHangBeautify.Zalo.DangKyThanhVien;
+using BanHangBeautify.Zalo.KetNoi_XacThuc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanHangBeautify.Zalo
+{
+    public class ZaloMapProfile: Profile
+    {
+        public ZaloMapProfile()
+        {
+            CreateMap<ZaloAuthorization, ZaloAuthorizationDto>().ReverseMap();
+            CreateMap<Zalo_KhachHangThanhVien, Zalo_KhachHangThanhVienDto>().ReverseMap();
+        }
+    }
+}
---
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Core/Entities/Zalo_KhachHangThanhVien.cs
@@ -0,0 +1,26 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanHangBeautify.Entities
+{
+    public class Zalo_KhachHangThanhVien: FullAuditedEntity<Guid>, IMustHaveTenant
+    {
+        public int TenantId { get; set; }
+        public Guid IdKhachHang { get;set; }
+        [ForeignKey(nameof(IdKhachHang))]
+        public DM_KhachHang DM_KhachHang { get; set; }
+        [MaxLength(500)]
+        public string TenDangKy { get; set; }
+        [MaxLength(100)]
+        public string SoDienThoaiDK { get; set; } 
+        public string ZOAUserId { get; set; }
+
+    }
+}
