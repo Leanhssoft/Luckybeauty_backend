e7d879d643e11a23d672d239a86c90b148f45dea
NhuongDT <inftech.ts91@gmail.com>
2023-12-08 08:44:01 +0700

    NhatKyGuiTinSMSDto: add {IdHoaDon, IdBooking}

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/SMS/Dto/NhatKyGuiTinSMSDto.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/SMS/Dto/NhatKyGuiTinSMSDto.cs
@@ -11,6 +11,8 @@ namespace BanHangBeautify.SMS.Dto
         public Guid IdHeThongSMS { get; set; }
         public Guid IdKhachHang { get; set; }
         public Guid IdChiNhanh { get; set; }
+        public Guid? IdHoaDon { get; set; }
+        public Guid? IdBooking { get; set; }
         public byte? IdLoaiTin { get; set; } = 1;
         public DateTime? ThoiGianTu { get; set; }
         public DateTime? ThoiGianDen { get; set; }
