19712af0f905526dfb6ae0f62d6f4547b5c59c9a
NhuongDT <inftech.ts91@gmail.com>
2023-12-09 15:11:29 +0700

    ChietKhauHoaDonItemDto: GiaTriChietKhau (string --> double)

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/ChietKhau/ChietKhauHoaDon/Dto/ChietKhauHoaDonItemDto.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/ChietKhau/ChietKhauHoaDon/Dto/ChietKhauHoaDonItemDto.cs
@@ -5,9 +5,10 @@ namespace BanHangBeautify.ChietKhau.ChietKhauHoaDon.Dto
     public class ChietKhauHoaDonItemDto
     {
         public Guid Id { get; set; }
-        public string GiaTriChietKhau { get; set; }
+        public Guid IdNhanVien { get; set; }
+        public double? GiaTriChietKhau { get; set; }
         public string ChungTuApDung { get; set; }
         public string GhiChu { get; set; }
-        public byte LoaiChietKhau { set; get; }
+        public byte? LoaiChietKhau { set; get; }
     }
 }
