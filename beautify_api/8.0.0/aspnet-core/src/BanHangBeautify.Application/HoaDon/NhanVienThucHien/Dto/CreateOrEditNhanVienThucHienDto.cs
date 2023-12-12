cb00e5f8edb81a8b6b4977d3f2dc9909809b9507
NhuongDT <inftech.ts91@gmail.com>
2023-12-12 09:03:09 +0700

    CreateOrEditNhanVienThucHienDto: HeSo (double --> float = real in SQL)

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/HoaDon/NhanVienThucHien/Dto/CreateOrEditNhanVienThucHienDto.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/HoaDon/NhanVienThucHien/Dto/CreateOrEditNhanVienThucHienDto.cs
@@ -6,14 +6,15 @@ namespace BanHangBeautify.HoaDon.NhanVienThucHien.Dto
     {
         public Guid Id { set; get; }
         public Guid IdNhanVien { set; get; }
-        public Guid IdHoaDon { set; get; }
-        public Guid IdChiTietHoaDon { set; get; }
-        public Guid IdQuyHoaDon { set; get; }
+        public Guid? IdHoaDon { set; get; }
+        public Guid? IdHoaDonChiTiet { set; get; }
+        public Guid? IdQuyHoaDon { set; get; }
         public double? PTChietKhau { set; get; }
         public double? TienChietKhau { set; get; }
-        public bool ChiaDeuChietKhau { set; get; }
-        public double? HeSo { set; get; }
-        public bool TinhHoaHongTruocCK { set; get; }
-        public byte LoaiChietKhau { set; get; }
+        public bool? ChiaDeuChietKhau { set; get; } = false;
+        public float? HeSo { set; get; }
+        public bool? TinhHoaHongTruocCK { set; get; } = false;
+        public byte? LoaiChietKhau { set; get; }
+        public string TenNhanVien { get; set; }
     }
 }
