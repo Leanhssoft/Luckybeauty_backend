11b2840763c81c7a384d9b597cae744e992cc87e
NhuongDT <inftech.ts91@gmail.com>
2023-12-12 08:55:43 +0700

    add NhanVienThucHienRepository

---
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/HoaDon/NhanVienThucHien/Repository/INhanVienThucHienRepository.cs
@@ -0,0 +1,16 @@
+﻿using BanHangBeautify.HoaDon.NhanVienThucHien.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BanHangBeautify.HoaDon.NhanVienThucHien.Repository
+{
+    public interface INhanVienThucHienRepository
+    {
+        Task<List<CreateOrEditNhanVienThucHienDto>> GetNhanVienThucHien_byIdHoaDon(Guid idHoaDon, Guid? idQuyHoaDon = null);
+        Task<List<CreateOrEditNhanVienThucHienDto>> GetNhanVienThucHien_byIdHoaDonChiTiet(Guid idHoaDonChiTiet);
+        Task<bool> UpdateNhanVienThucHien_byIdHoaDon(int? tenantId,  Guid idHoaDon, List<CreateOrEditNhanVienThucHienDto> lstNV = null);
+    }
+}
