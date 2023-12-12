14d95e65da702e5ec7976ed21ac3d783fe797303
NhuongDT <inftech.ts91@gmail.com>
2023-12-12 09:03:29 +0700

    rename func GetHoaHongNV_theoHoaDon --> GetHoaHongNVienSetup_theoLoaiChungTu

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/ChietKhau/ChietKhauHoaDon/Repository/IChietKhauHoaDonRepository.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/ChietKhau/ChietKhauHoaDon/Repository/IChietKhauHoaDonRepository.cs
@@ -9,6 +9,6 @@ namespace BanHangBeautify.ChietKhau.ChietKhauHoaDon.Repository
     public interface IChietKhauHoaDonRepository
     {
         public Task<PagedResultDto<ChietKhauHoaDonItemDto>> GetAll(PagedRequestDto input, int tenatId, Guid? idChiNhanh);
-        Task<List<ChietKhauHoaDonItemDto>> GetHoaHongNV_theoHoaDon(Guid idChiNhanh, Guid idNhanVien, string loaiChungTu = "1");
+        Task<List<ChietKhauHoaDonItemDto>> GetHoaHongNVienSetup_theoLoaiChungTu(Guid idChiNhanh, Guid idNhanVien, string loaiChungTu = "1");
     }
 }
