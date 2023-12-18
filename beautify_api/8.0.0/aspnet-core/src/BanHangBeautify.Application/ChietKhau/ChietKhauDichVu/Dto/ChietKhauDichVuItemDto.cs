5f20fbba0dcc5c659b7e14920378b100250bb0d1
NhuongDT <inftech.ts91@gmail.com>
2023-12-18 09:10:16 +0700

    add class (ChietKhauDichVuItemDto_TachRiengCot)
    ChietKhauDichVuItemDto (Id: Guid --> Guid ?)

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/ChietKhau/ChietKhauDichVu/Dto/ChietKhauDichVuItemDto.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/ChietKhau/ChietKhauDichVu/Dto/ChietKhauDichVuItemDto.cs
@@ -4,7 +4,7 @@ namespace BanHangBeautify.ChietKhau.ChietKhauDichVu.Dto
 {
     public class ChietKhauDichVuItemDto
     {
-        public Guid Id { get; set; }
+        public Guid? Id { get; set; }
         public string TenDichVu { get; set; }
         public string TenNhomDichVu { get; set; }
         public double GiaTri { get; set; }
@@ -13,5 +13,22 @@ namespace BanHangBeautify.ChietKhau.ChietKhauDichVu.Dto
         public double? HoaHongYeuCauThucHien { get; set; }
         public double? HoaHongTuVan { get; set; }
         public double GiaDichVu { get; set; }
+        public string TenNhanVien { get; set; }
+    }
+    
+    public class ChietKhauDichVuItemDto_TachRiengCot
+    {
+        public Guid? IdNhanVien { get; set; }
+        public Guid? IdDonViQuiDoi { get; set; }
+        public double? GiaDichVu { get; set; }
+        public string TenNhanVien { get; set; }
+        public string TenDichVu { get; set; }
+        public string TenNhomDichVu { get; set; }
+        public double? HoaHongThucHien { get; set; }
+        public double? HoaHongYeuCauThucHien { get; set; }
+        public double? HoaHongTuVan { get; set; }
+        public bool? LaPhanTram_HoaHongThucHien { get; set; }
+        public bool? LaPhanTram_HoaHongYeuCauThucHien { get; set; }
+        public bool? LaPhanTram_HoaHongTuVan { get; set; }
     }
 }
