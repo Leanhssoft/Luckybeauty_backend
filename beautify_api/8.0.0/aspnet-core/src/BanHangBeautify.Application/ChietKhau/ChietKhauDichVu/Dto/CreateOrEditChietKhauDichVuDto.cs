20b85482079a8dcfa3651e09e3bb6a0560b6f6d7
NhuongDT <inftech.ts91@gmail.com>
2023-12-18 09:10:32 +0700

    add class ChietKhauDichVuDto_AddMultiple

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/ChietKhau/ChietKhauDichVu/Dto/CreateOrEditChietKhauDichVuDto.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/ChietKhau/ChietKhauDichVu/Dto/CreateOrEditChietKhauDichVuDto.cs
@@ -5,10 +5,20 @@ namespace BanHangBeautify.ChietKhau.ChietKhauDichVu.Dto
 {
     public class CreateOrEditChietKhauDichVuDto
     {
-        public Guid Id { set; get; }
+        public Guid? Id { set; get; }
         public Guid? IdChiNhanh { set; get; }
         public List<Guid> IdNhanViens { set; get; }
         public Guid IdDonViQuiDoi { set; get; }
+        public byte? LoaiChietKhau { set; get; } = 1;
+        public double? GiaTri { set; get; } = 0;
+        public bool? LaPhanTram { set; get; }
+    }
+
+    public class ChietKhauDichVuDto_AddMultiple
+    {
+        public Guid? IdChiNhanh { set; get; }
+        public List<Guid> IdNhanViens { set; get; }
+        public List<Guid> IdDonViQuyDois { set; get; }
         public byte? LoaiChietKhau { set; get; } = 1;
         public double? GiaTri { set; get; } = 0;
         public bool? LaPhanTram { set; get; }
