eefe6e4d97fbcf270bb1401f755e58d1ee55f731
NhuongDT <inftech.ts91@gmail.com>
2023-12-18 09:11:27 +0700

    KH_CheckIn: add comment for (trangThai)

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Core/Entities/KH_CheckIn.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Core/Entities/KH_CheckIn.cs
@@ -18,6 +18,11 @@ namespace BanHangBeautify.Entities
         public DateTime DateTimeCheckIn { get; set; }// ngay check in yyyy-mm-dd hh:mm:ss
         [MaxLength(4000)]
         public string GhiChu { get; set; } = string.Empty;
-        public int TrangThai { get; set; } = 1; // 0. Xóa khách checkin (do thêm sai), 1.dang check in & đang làm dịch vụ (mặc định), 2. đã check in & làm xong dịch vụ, 3.Khách đến check in nhưng đợi lâu quá (Cancel)
+        // 0. Xóa khách checkin (do thêm sai),
+        // 1. Đã check in - chưa chọn dịch vụ (mặc định: khách đến và check in) - Đang chờ
+        // 2. Đã check in - đã chọn dịch vụ (Đang thực hiện)
+        // 2. Đã check in & làm xong dịch vụ - Hoàn thành
+        // 3. Khách đến check in nhưng đợi lâu quá (Cancel)
+        public int TrangThai { get; set; } = 1; 
     }
 }
