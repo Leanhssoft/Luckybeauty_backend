cfc09e84e461ae6cc31177bfdbe52367d651b9ca
NhuongDT <inftech.ts91@gmail.com>
2023-12-23 08:46:41 +0700

    add class PageNhatKyChuyenTienDto

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/SMS/Dto/LichSuNap_ChuyenTienDto.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/SMS/Dto/LichSuNap_ChuyenTienDto.cs
@@ -9,11 +9,19 @@ namespace BanHangBeautify.SMS.Dto
 {
     public class LichSuNap_ChuyenTienDto
     {
-        public Guid? IdPhieuNapTien { get; set; } // Lấy từ bảng QuyHoaDon của HOST
+        public Guid? Id { get; set; } 
+        public Guid? IdPhieuNapTien { get; set; } // Lấy từ bảng QuyHoaDon của HOST (neu naptien)
         public DateTime ThoiGianNap_ChuyenTien { get; set; }
         public long? IdNguoiChuyenTien { get; set; } // Nếu nạp tiền: null, Nếu chuyển tiền: lưu IdNhanVien chuyển tiền
         public long? IdNguoiNhanTien { get; set; }// Nếu nạp tiền: lưu IdTaiKhoan Admin, Nếu nhận tiền: lưu IdNhanVien được chuyển tiền
         public double? SoTienChuyen_Nhan { get; set; } = 0;
         public string NoiDungChuyen_Nhan { get; set; }
+    } 
+    public class PageNhatKyChuyenTienDto: LichSuNap_ChuyenTienDto
+    {
+        public string UserChuyenTien { get; set; }
+        public string UserNhanTien { get; set; }
+        public string LoaiPhieu { get; set; }// chuyen, nhan
+        public DateTime? CreationTime { get; set; }
     }
 }
