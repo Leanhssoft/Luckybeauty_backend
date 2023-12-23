5e8769f1cc1d2beac2486dea50631593e3f768e7
NhuongDT <inftech.ts91@gmail.com>
2023-12-23 08:47:45 +0700

    add class UserProfileDto

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/Users/Dto/ProfileDto.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/Users/Dto/ProfileDto.cs
@@ -19,5 +19,17 @@ namespace BanHangBeautify.Users.Dto
         public string NoiCap { get; set; }
         public string DiaChi { get; set; }
         public string Password { get; set; }
+       
+    }
+
+    public class UserProfileDto : ProfileDto
+    {
+        public string TenNhanVien { get; set; }// tennhanvien có thể # name, surname trong bang User (vi NV co the bi cap nhat)
+        public string TenChiNhanh { get; set; }// chi nhanh mac dinh
+        public bool? IsActive { get; set; }
+        public bool? IsAdmin { get; set; }
+        public DateTime CreationTime { get; set; }
+        public string RoleNames { get; set; }
+        public string TxtTrangThai { get; set; }// get from IsActive
     }
 }
