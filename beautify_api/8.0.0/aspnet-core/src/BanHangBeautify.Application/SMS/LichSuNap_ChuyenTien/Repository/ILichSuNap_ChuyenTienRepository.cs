258f3ee8aca8ea6b7a93cffa22b0e29783e44c28
NhuongDT <inftech.ts91@gmail.com>
2023-12-23 08:46:30 +0700

    add ILichSuNap_ChuyenTienRepository (GetAllNhatKyChuyenTien)

---
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/SMS/LichSuNap_ChuyenTien/Repository/ILichSuNap_ChuyenTienRepository.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Application.Services.Dto;
+using BanHangBeautify.SMS.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BanHangBeautify.Configuration.Common.CommonClass;
+
+namespace BanHangBeautify.SMS.LichSuNap_ChuyenTien.Repository
+{
+    public interface ILichSuNap_ChuyenTienRepository
+    {
+        Task<PagedResultDto<PageNhatKyChuyenTienDto>> GetAllNhatKyChuyenTien(ParamSearch param);
+    }
+}
