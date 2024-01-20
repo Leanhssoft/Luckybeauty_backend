5ea83122707d5751ee7471b19da2edfeb938473e
NhuongDT <inftech.ts91@gmail.com>
2024-01-20 14:34:50 +0700

    KhachHangDetailDto:
    NhomKhach --> TenNhomKhach
    LoaiKhach --> TenLoaiKhachHang
    NguonKhach --> TenNguonKhach
    DiemThuong --> TongTichDiem
    NgaySinh: string --> Datetime

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/KhachHang/KhachHang/Dto/KhachHangDetailDto.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/KhachHang/KhachHang/Dto/KhachHangDetailDto.cs
@@ -5,18 +5,29 @@ namespace BanHangBeautify.KhachHang.KhachHang.Dto
     public class KhachHangDetailDto
     {
         public Guid Id { get; set; }
+        public Guid? IdNhomKhach { get; set; }
+        public Guid? IdLoaiKhach { get; set; }
+        public Guid? IdNguonKhach { get; set; }
         public string Avatar { get; set; }
         public string MaKhachHang { get; set; }
         public string TenKhachHang { get; set; }
         public string SoDienThoai { get; set; }
-        public float DiemThuong { get; set; }
+        public float? TongTichDiem { get; set; }
         public string MaSoThue { get; set; }
         public string Email { get; set; }
         public string DiaChi { get; set; }
         public string GioiTinh { get; set; }
-        public string NgaySinh { get; set; }
-        public string LoaiKhach { get; set; }
-        public string NhomKhach { get; set; }
-        public string NguonKhach { get; set; }
+        public DateTime? NgaySinh { get; set; }
+        public string TenLoaiKhachHang { get; set; }
+        public string TenNhomKhach { get; set; }
+        public string TenNguonKhach { get; set; }
+    }
+    public class CustomerDetail_FullInfor : KhachHangDetailDto
+    {
+        public int? SoLanBooking { get; set; } = 0;
+        public int? SoLanCheckIn { get; set; } = 0;
+        public double? TongChiTieu { get; set; } = 0;
+        public double? ConNo { get; set; } = 0;
+        public DateTime? cuocHenGanNhat { get; set; }
     }
 }
