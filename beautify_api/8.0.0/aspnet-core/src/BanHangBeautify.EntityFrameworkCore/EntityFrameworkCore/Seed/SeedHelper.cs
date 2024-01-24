a22fe405b1e39d453d913ff8d31fce07d7c0eb19
NhuongDT <inftech.ts91@gmail.com>
2024-01-24 17:28:05 +0700

    add Seed (DM_NganHang)

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.EntityFrameworkCore/EntityFrameworkCore/Seed/SeedHelper.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.EntityFrameworkCore/EntityFrameworkCore/Seed/SeedHelper.cs
@@ -2,6 +2,7 @@
 using Abp.Domain.Uow;
 using Abp.EntityFrameworkCore.Uow;
 using Abp.MultiTenancy;
+using BanHangBeautify.EntityFrameworkCore.Seed.Custom;
 using BanHangBeautify.EntityFrameworkCore.Seed.Host;
 using BanHangBeautify.EntityFrameworkCore.Seed.LoaiHangHoa;
 using BanHangBeautify.EntityFrameworkCore.Seed.Tenants;
@@ -37,6 +38,19 @@ namespace BanHangBeautify.EntityFrameworkCore.Seed
 
             //Setting tenants
             new DefaultTenantSettings(context).Create();
+
+            new DM_NganHangBuilder(context).Create();
+        }
+
+        public static void SeedTenant(IIocResolver iocResolver)
+        {
+            WithDbContext<SPADbContext>(iocResolver, SeedTenant);
+        }
+
+        public static void SeedTenant(SPADbContext context)
+        {
+            context.SuppressAutoSetTenantId = true;
+            new DM_NganHangBuilder(context).Create();
         }
 
         private static void WithDbContext<TDbContext>(IIocResolver iocResolver, Action<TDbContext> contextAction)
