26ad8bdd4e18516ca24a724a3373d6e15fa6d57b
NhuongDT <inftech.ts91@gmail.com>
2024-03-29 08:35:25 +0700

    Zalo_KhachHangThanhVien: remove + add column

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/Zalo/DangKy_ThanhVien/IZalo_KhachHangThanhVienAppService.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/Zalo/DangKy_ThanhVien/IZalo_KhachHangThanhVienAppService.cs
@@ -11,6 +11,6 @@ namespace BanHangBeautify.Zalo.DangKy_ThanhVien
     {
         Task<Zalo_KhachHangThanhVienDto> DangKyThanhVienZOA(Zalo_KhachHangThanhVienDto dto);
         Task<Zalo_KhachHangThanhVienDto> UpdateThanhVienZOA(Zalo_KhachHangThanhVienDto dto);
-        bool CheckExistZaloUserId(string zaloUserId);
+        Guid? GetId_fromZOAUserId(string zaloUserId);
     }
 }
--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/Zalo/DangKy_ThanhVien/Zalo_KhachHangThanhVienDto.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/Zalo/DangKy_ThanhVien/Zalo_KhachHangThanhVienDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,11 +10,46 @@ namespace BanHangBeautify.Zalo.DangKyThanhVien
     public class Zalo_KhachHangThanhVienDto
     {
         public Guid? Id { get; set; }
-        public string TenDangKy { get; set; }
-        public string SoDienThoaiDK { get; set; }
+
+        [JsonProperty(PropertyName = "user_id")]
         public string ZOAUserId { get; set; }
-        public string DiaChi { get; set; }
-        public string TenTinhThanh { get; set; }
-        public string TenQuanHuyen { get; set; }
+
+        [JsonProperty(PropertyName = "display_name")]
+        public string DisplayName { get; set; }
+
+        [JsonProperty(PropertyName = "user_id_by_app")]
+        public string UserIdByApp { get; set; }
+
+        [JsonProperty(PropertyName = "user_is_follower")]
+        public bool? UserIsFollower { get; set; }// khách hàng quan tâm/chưa quan tâm OA
+
+        [JsonProperty(PropertyName = "avatar")]
+        public string Avatar { get; set; }
+    }
+    //public class Zalo_KhachHangThanhVienDto2
+    //{
+    //    public string user_id { get; set; }
+    //    public string user_alias { get; set; }
+    //    public string display_name { get; set; }
+    //    public string user_id_by_app { get; set; }
+    //    public bool user_is_follower { get; set; }
+    //    public bool is_sensitive { get; set; }
+    //    public string avatar { get; set; }
+    //}
+
+    public class ResultDataZaloCommon<T>
+    {
+        public int error;
+        public string message;
+        public T data;
+    } 
+   
+    public class ZaloUserShareInforDto
+    {
+        public string name { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string district { get; set; }
     }
 }
