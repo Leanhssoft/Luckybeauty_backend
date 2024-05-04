ffc6671f7ba3a1696aac524489feebf8de72542d
NhuongDT <inftech.ts91@gmail.com>
2024-05-04 14:28:50 +0700

    PageHoaDonDto: add {soDienThoai} --> bind at modal (thongTinHoaDon)

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/HoaDon/HoaDon/Dto/PageHoaDonDto.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/HoaDon/HoaDon/Dto/PageHoaDonDto.cs
@@ -3,6 +3,7 @@
     public class PageHoaDonDto : CreateHoaDonDto
     {
         public string UserName { get; set; }// user lapphieu
+        public string SoDienThoai { get; set; }
         public string MaKhachHang { get; set; }
         public string TenKhachHang { get; set; }
         public string Avatar { get; set; }
