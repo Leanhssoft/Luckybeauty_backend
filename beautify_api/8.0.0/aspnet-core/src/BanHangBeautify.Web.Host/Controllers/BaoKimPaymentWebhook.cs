56ee48a2c27b7481459f516f87e7055ce7c36e8c
NhuongDT <inftech.ts91@gmail.com>
2024-05-20 15:56:16 +0700

    commit đế test api (not important)

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Web.Host/Controllers/BaoKimPaymentWebhook.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Web.Host/Controllers/BaoKimPaymentWebhook.cs
@@ -24,23 +24,21 @@ namespace BanHangBeautify.Web.Host.Controllers
         }
         [HttpPost]
         [Route("transaction-notification")]
-        public async Task<ResponseThongBaoGiaoDich> ThongBaoGiaoDich()
+        public async Task<bool> ThongBaoGiaoDich()
         {
             using (StreamReader reader = new StreamReader(HttpContext.Request.Body, Encoding.UTF8))
             {
                 var body = await reader.ReadToEndAsync();
                 var data = JsonConvert.DeserializeObject<ResponseThongBaoGiaoDich>(body);
                 var dataSign = $@"{data.RequestId}|{data.RequestTime}|{data.PartnerCode}|{data.AccNo}|{data.ClientIdNo}{data.TransId}|{data.TransAmount}|{data.TransTime}|{data.BefTransDebt}|{data.AffTransDebt}|{data.AccountType}|{data.OrderId}";
-                var sign = _baokimPaymentAppService.CreateSignature(dataSign);
+                //var sign = _baokimPaymentAppService.CreateSignature(dataSign);
 
-                if (!_baokimPaymentAppService.VerifySignature(dataSign, sign))
+                if (!_baokimPaymentAppService.VerifySignature(dataSign, data.Signature))
                 {
-                    return data;
+                    return false;
                 }
-                await _baokimPaymentAppService.GuiLaiThongTinGiaoDich_BaoKim(data);
             }
-            //return Ok();
-            return null;
+            return true;
         }
     }
 }
