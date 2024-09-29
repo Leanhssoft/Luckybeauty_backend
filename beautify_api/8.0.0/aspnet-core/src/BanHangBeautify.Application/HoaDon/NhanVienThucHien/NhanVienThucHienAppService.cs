336da639f6235c2356d49a1bf5732ad4828baeaf
NhuongDT <inftech.ts91@gmail.com>
2024-09-29 09:12:21 +0700

    remove func not use & add func UpdateTienChietKhau_forNhanVien_whenUpdateCTHD

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/HoaDon/NhanVienThucHien/NhanVienThucHienAppService.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/HoaDon/NhanVienThucHien/NhanVienThucHienAppService.cs
@@ -29,64 +29,7 @@ namespace BanHangBeautify.HoaDon.NhanVienThucHien
             _nvThucHien = repository;
             _nvthRepository = nvthRepository;
         }
-        [AbpAuthorize(PermissionNames.Pages_NhanVienThucHien_Create)]
-        public async Task<bool> InsertListNVThucHien_DichVu(List<HoaDonChiTietDto> lstCTHD)
-        {
-            try
-            {
-                List<BH_NhanVienThucHien> lstNVTH = new List<BH_NhanVienThucHien>();
-                foreach (var cthd in lstCTHD)
-                {
-                    foreach (var nvth in cthd.nhanVienThucHien)
-                    {
-                        BH_NhanVienThucHien nvNew = ObjectMapper.Map<BH_NhanVienThucHien>(nvth);
-                        nvNew.Id = Guid.NewGuid();
-                        nvNew.TenantId = AbpSession.TenantId ?? 1;
-                        nvNew.CreatorUserId = AbpSession.UserId;
-                        nvNew.CreationTime = DateTime.Now;
-                        lstNVTH.Add(nvNew);
-                    }
-                }
-                await _nvThucHien.InsertRangeAsync(lstNVTH);
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-        }
-        [HttpPost]
-        [AbpAuthorize(PermissionNames.Pages_NhanVienThucHien_Edit)]
-        public async Task<bool> UpdateNVThucHienDichVu([FromBody] JObject data, Guid idHoaDonChiTiet)
-        {
-            try
-            {
-                List<NhanVienThucHienDto> lstNV = data["NhanViens"].ToObject<List<NhanVienThucHienDto>>();
 
-                // upadate status & add again
-                _nvThucHien.GetAllIncluding(x => x.IdHoaDonChiTiet == idHoaDonChiTiet).ToList()
-                    .ForEach(x => { x.IsDeleted = true; x.DeletionTime = DateTime.Now; x.DeleterUserId = AbpSession.UserId; });
-
-                List<BH_NhanVienThucHien> lstNVAdd = new();
-                foreach (var item in lstNV)
-                {
-                    var objNew = ObjectMapper.Map<BH_NhanVienThucHien>(item);
-                    objNew.Id = Guid.NewGuid();
-                    objNew.CreationTime = DateTime.Now;
-                    objNew.IdHoaDonChiTiet = idHoaDonChiTiet;
-                    objNew.CreatorUserId = AbpSession.UserId;
-                    objNew.TenantId = AbpSession.TenantId ?? 1;
-                    objNew.IsDeleted = false;
-                    lstNVAdd.Add(objNew);
-                }
-                await _nvThucHien.InsertRangeAsync(lstNVAdd);
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
-        }
         [HttpPost]
         [AbpAuthorize(PermissionNames.Pages_NhanVienThucHien_Delete)]
         public async Task<NhanVienThucHienDto> Delete(Guid id)
@@ -141,17 +84,6 @@ namespace BanHangBeautify.HoaDon.NhanVienThucHien
                 return ObjectMapper.Map<CreateOrEditNhanVienThucHienDto>(data);
             }
             return new CreateOrEditNhanVienThucHienDto();
-        }
-        public async Task<PagedResultDto<NhanVienThucHienDto>> GetAll(PagedRequestDto input)
-        {
-            input.SkipCount = input.SkipCount > 1 ? (input.SkipCount - 1) * input.MaxResultCount : 0;
-            input.Keyword = string.IsNullOrEmpty(input.Keyword) ? "" : input.Keyword;
-            PagedResultDto<NhanVienThucHienDto> result = new PagedResultDto<NhanVienThucHienDto>();
-            var lstData = await _nvThucHien.GetAll().Where(x => x.IsDeleted == false && x.TenantId == (AbpSession.TenantId ?? 1)).OrderByDescending(x => x.CreationTime).ToListAsync();
-            result.TotalCount = lstData.Count;
-            lstData = lstData.Skip(input.SkipCount).Take(input.MaxResultCount).ToList();
-            result.Items = ObjectMapper.Map<List<NhanVienThucHienDto>>(lstData);
-            return result;
         }
         [HttpGet]
         public async Task<List<CreateOrEditNhanVienThucHienDto>> GetNhanVienThucHien_byIdHoaDon(Guid idHoaDon, Guid? idQuyHoaDon = null)
@@ -237,6 +169,18 @@ namespace BanHangBeautify.HoaDon.NhanVienThucHien
                     await _nvThucHien.InsertAsync(objNew);
                 }
                 return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        [HttpGet]
+        public async Task<bool> UpdateTienChietKhau_forNhanVien_whenUpdateCTHD(Guid idHoaDonChiTiet, double soLuoongOld)
+        {
+            try
+            {
+                return await _nvthRepository.UpdateTienChietKhau_forNhanVien_whenUpdateCTHD(idHoaDonChiTiet, soLuoongOld);
             }
             catch (Exception)
             {
