20cc8dcd9207d3f65aac4addd7473731fbd10f31
NhuongDT <inftech.ts91@gmail.com>
2024-09-29 09:13:29 +0700

    QuyHoaDonViewItemDto: change name props (sLoaiPhieu --> loaiPhieu, STrangThai --> txtTrangThai)

--- beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/Quy/DM_QuyHoaDon/Dto/QuyHoaDonViewItemDto.cs
+++ beautify_api/8.0.0/aspnet-core/src/BanHangBeautify.Application/Quy/DM_QuyHoaDon/Dto/QuyHoaDonViewItemDto.cs
@@ -2,11 +2,11 @@
 {
     public class QuyHoaDonViewItemDto : QuyHoaDonDto
     {
-        public string SLoaiPhieu { get; set; }// 11.thu/12.chi
+        public string LoaiPhieu { get; set; }// 11.thu/12.chi
         public string SHinhThucThanhToan { get; set; }
         public string MaNguoiNop { get; set; }
         public string TenNguoiNop { get; set; }
-        public string STrangThai { get; set; }
+        public string TxtTrangThai { get; set; }
         public string TenKhoanThuChi { get; set; }
         public int? TrangThai { get; set; } = 1;
     }
